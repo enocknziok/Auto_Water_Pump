943a17991b2cf88d83b435632989a3290305f796
noisycarlos <30759765+noisycarlos@users.noreply.github.com>
2018-06-27 12:53:11 -0700

    Initial commit

---
+++ Auto_Water_Pump.ino
@@ -0,0 +1,63 @@
+//A0 = Sensor In
+//A5 = Relay Out
+
+int input = A0;
+int output = A5;
+
+int state = 1;
+
+int maxSecondsOn = 120;
+int minSecondsOn = 30;
+
+int secondsOn = 0;
+
+void setup()
+{
+  pinMode(input, INPUT_PULLUP);
+  pinMode(output, OUTPUT); 
+
+  Serial.begin(9600);
+
+  digitalWrite(output, 1);
+  delay(500);
+  digitalWrite(output, 0);
+
+  Serial.println("Started");
+
+}
+
+void loop()
+{
+
+  int newState = !digitalRead(input);
+
+   Serial.print("Output State: ");
+   Serial.print(state);
+   Serial.print("  Sensor state: ");
+   Serial.print(newState);
+   Serial.print("  Seconds On: ");
+   Serial.println(secondsOn);
+
+  if(state == 1)
+  {
+    secondsOn = secondsOn + 1;
+
+    if(secondsOn > maxSecondsOn)
+    {
+      newState = 0;     
+      
+    }
+    if(secondsOn < minSecondsOn)
+        newState = 1;
+  }
+
+  if(newState != state) //Status Changed
+  {
+    Serial.print("Changing Output to ");
+    Serial.println(newState);
+    state = newState;
+    secondsOn = 0;
+    digitalWrite(output, state);
+  }
+  delay(1000);
+}
